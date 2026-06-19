61df0c22f2aa48fc93bbb1ebe42de0d82f7f0cb9
Labrynth King <labrynthking@proton.me>
2026-06-19 22:12:56 +0530

    Init: Basic Functionality

---
+++ InspectToolsOrigins/Plugin.cs
@@ -0,0 +1,96 @@
+﻿// Inspect Tools: Origins => A First Equip Animation Replay Mod For Subnautica & Subnautica: Below Zero
+// Copyright (C)  2026  LabrynthKing
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+using System.Collections;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using Nautilus.Handlers;
+using UnityEngine;
+
+namespace InspectToolsOrigins;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+[BepInDependency("com.snmodding.nautilus")]
+public class Plugin : BaseUnityPlugin
+{
+    // Config Keybind
+    public static GameInput.Button InspectButton = EnumHandler
+        .AddEntry<GameInput.Button>("InspectButton_Bind")
+        .CreateInput().WithKeyboardBinding(GameInputHandler.Paths.Keyboard.I)
+        .WithControllerBinding(GameInputHandler.Paths.Gamepad.DpadRight)
+        .AvoidConflicts()
+        .WithCategory(PluginInfo.PLUGIN_NAME);
+
+    private bool _isInspecting;
+
+    public new static ManualLogSource Logger { get; private set; }
+
+    private static Assembly Assembly { get; } = Assembly.GetExecutingAssembly();
+
+    private void Awake()
+    {
+        Logger = base.Logger;
+
+        // Register Localization
+        LanguageHandler.RegisterLocalizationFolder();
+
+        Harmony.CreateAndPatchAll(Assembly, $"{PluginInfo.PLUGIN_GUID}");
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+    }
+
+    private void Update()
+    {
+        if (!GameInput.IsInitialized || uGUI_PDA.main?.tabOpen != PDATab.None ||
+            Player.main is null || Inventory.main is null || (DevConsole.instance?.state ?? true)) return;
+
+        if (_isInspecting) return;
+
+        if (!GameInput.GetButtonDown(InspectButton)) return;
+
+        var heldTool = Inventory.main.GetHeldTool();
+        if (heldTool is null || !heldTool.hasFirstUseAnimation) return;
+
+        var techy = heldTool.pickupable.GetTechType();
+        if (techy is TechType.None) return;
+
+        var quickSlots = Inventory.main.quickSlots;
+
+        var item = heldTool.pickupable.inventoryItem;
+        var slot = quickSlots.GetSlotByItem(item);
+        if (slot == -1) return;
+
+        StartCoroutine(InspectRoutine(quickSlots, slot, techy, heldTool.holsterTime));
+    }
+
+    private IEnumerator InspectRoutine(QuickSlots quickSlots, int slot, TechType techy, float holsterTime)
+    {
+        _isInspecting = true;
+        Logger.LogInfo($"Inspecting tool: {techy}");
+
+        if (Player.main.usedTools.Contains(techy))
+            Player.main.usedTools.Remove(techy);
+
+        quickSlots.DeselectImmediate();
+
+        yield return new WaitForSeconds(holsterTime + 0.01f);
+
+        quickSlots.SelectImmediate(slot);
+
+        _isInspecting = false;
+    }
+}
---
+++ InspectToolsZero/Config.cs
@@ -0,0 +1,28 @@
+// Inspect Tools: Origins => A First Equip Animation Replay Mod For Subnautica & Subnautica: Below Zero
+// Copyright (C)  2026  LabrynthKing
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+using Nautilus.Json;
+using Nautilus.Options.Attributes;
+using UnityEngine;
+
+namespace InspectToolsZero;
+
+[Menu("Inspect Tools: Zero")]
+public class Config : ConfigFile
+{
+    [Keybind(LabelLanguageId = "OptionInspectButton_Bind", TooltipLanguageId = "OptionDesc_InspectButton_Bind")]
+    public KeyCode InspectKeyBind = KeyCode.I;
+}
